deb3cc484625b1a6432257121feee1c4b76bd119
Akpemie Darlington Alloh <xbladex16@gmail.com>
2023-12-16 19:43:57 +0100

    Add files via upload

---
+++ log.sql
@@ -0,0 +1,33 @@
+
+-- *** The Lost Letter ***
+SELECT * FROM "addresses"
+WHERE "address" = "900 Somerville Avenue";
+
+SELECT * FROM "packages"
+WHERE "from_address_id" = "432";
+
+SELECT * FROM "scans"
+WHERE "address_id" = "854"
+AND "package_id" = "384";
+
+SELECT * FROM "addresses"
+WHERE "id" = "854";
+
+-- *** The Devious Delivery ***
+SELECT * FROM packages WHERE from_address_id IS NULL;
+
+SELECT * FROM scans WHERE package_id = 5098;
+
+SELECT * FROM addresses WHERE id =  348;
+
+-- *** The Forgotten Gift ***
+SELECT * FROM "addresses" WHERE "address" = "109 Tileston Street";
+
+SELECT * FROM "packages"
+WHERE "from_address_id" = "9873";
+
+SELECT * FROM "scans" WHERE "package_id" = 9523;
+
+SELECT * FROM "addresses" WHERE "id" = 7432;
+
+SELECT * FROM "drivers" WHERE "id" = 17;
